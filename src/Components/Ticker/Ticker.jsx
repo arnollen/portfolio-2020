1f47814cda9b87b865fcfe38b42d7ae34d6e82a6
Anthony Nollen <anthonynollen@gmail.com>
2023-04-06 15:32:36 -0500

    Update Ticker.jsx

--- src/Components/Ticker/Ticker.jsx
+++ src/Components/Ticker/Ticker.jsx
@@ -1,37 +1,87 @@
-import React from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Ticker.scss';
 
-function Ticker(props) {
-  const {
-    header, body, body2, gif,
-  } = props;
+class Ticker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      section: '',
+      scrollText: 'SOME TEXT GOES HERE AND IT WILL SCROLL ACROSS THE SCREEN',
+      scrollText2: 'SOME TEXT GOES HERE AND IT WILL SCROLL ACROSS THE SCREEN',
+      scrollGif: 'https://media.giphy.com/media/111ebonMs90YLu/giphy.gif',
+    };
+  }
 
-  const tickerSections = [
-    { content: body },
-    { content: body2 },
-    { content: body },
-    { content: <img src={gif} alt="gif" title="gif" /> },
-    { content: body2 },
-    { content: body },
-    { content: body2 },
-    { content: body },
-    { content: <img src={gif} alt="gif" title="gif" /> },
-    { content: body2 },
-  ];
+  componentDidMount() {
+    const myProps = this.props;
 
-  return (
-    <div id="ticker--container">
-      <div id="ticker-content-cotainer">
-        {tickerSections.map((section, index) => (
-          <div className="ticker-content" key={index}>
-            {index % 3 === 0 && <h1>{header}</h1>}
-            <p>{section.content}</p>
+    this.setState({
+      section: myProps.header,
+      scrollText: myProps.body,
+      scrollText2: myProps.body2,
+      scrollGif: myProps.gif,
+    });
+  }
+
+  render() {
+    const {
+      scrollText, scrollText2, section, scrollGif,
+    } = this.state;
+    return (
+      <div id="ticker--container">
+        <div id="ticker-content-cotainer">
+          <div className="ticker-content">
+            <h1>{ section }</h1>
           </div>
-        ))}
+          <div className="ticker-content">
+            <p>{ scrollText }</p>
+            <p>{ scrollText2 }</p>
+            <p>{ scrollText }</p>
+          </div>
+          <div className="ticker-content">
+            <h1>{ section }</h1>
+          </div>
+          <div className="ticker-content">
+            <img src={scrollGif} alt="gif" title="gif" />
+          </div>
+          <div className="ticker-content">
+            <h1>{ section }</h1>
+          </div>
+          <div className="ticker-content">
+            <p>{ scrollText2 }</p>
+            <p>{ scrollText }</p>
+            <p>{ scrollText2 }</p>
+          </div>
+
+          <div className="ticker-content">
+            <h1>{ section }</h1>
+          </div>
+          <div className="ticker-content">
+            <p>{ scrollText }</p>
+            <p>{ scrollText2 }</p>
+            <p>{ scrollText }</p>
+          </div>
+          <div className="ticker-content">
+            <h1>{ section }</h1>
+          </div>
+          <div className="ticker-content">
+            <img src={scrollGif} alt="gif" title="gif" />
+          </div>
+          <div className="ticker-content">
+            <h1>{ section }</h1>
+          </div>
+          <div className="ticker-content">
+            <p>{ scrollText2 }</p>
+            <p>{ scrollText }</p>
+            <p>{ scrollText2 }</p>
+          </div>
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Ticker.propTypes = {
