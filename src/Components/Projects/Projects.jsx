6564174900f2ba62ba94bfe14ff0eec3f7692c4c
Anthony Nollen <anthonynollen@gmail.com>
2023-08-14 15:13:45 -0500

    launched

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -14,7 +14,7 @@ import Archive from '../Archive/Archive';
 import About from '../About/About';
 import Spotify from '../Spotify/Spotify';
 import Footer from '../Footer/Footer';
-// import RandomContent from '../RandomContent/RandomContent';
+import RandomContent from '../RandomContent/RandomContent';
 import { thecontext } from '../Context/Context';
 import './Projects.scss';
 
@@ -113,9 +113,9 @@ class Projects extends Component {
 
             <Spotify />
 
-             {/* <Ticker header="Art Fart" body="The pencil is one of mans simplest tools" body2="Nothing like making a little something from nothing." gif="https://media.giphy.com/media/Pv3cmW2HAmvsc/giphy.gif" />
+             <Ticker header="Art Fart" body="The pencil is one of mans simplest tools" body2="Nothing like making a little something from nothing." gif="https://media.giphy.com/media/Pv3cmW2HAmvsc/giphy.gif" />
 
-            <RandomContent /> */}
+            <RandomContent />
 
             <Footer />
         </div>
--- src/Components/RandomContent/RandomContent.jsx
+++ src/Components/RandomContent/RandomContent.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
+// import ReactDOM from 'react-dom';
+// import ReactGA from 'react-ga';
 import './RandomContent.scss';
 import zineData from '../../Data/zine.json';
 
@@ -12,41 +12,46 @@ class RandomContent extends Component {
     this.state = { zinePages: zineData, rightPage: '', leftPage: '' };
   }
 
-  componentDidMount = () => {
+  componentDidMount() {
     this.loadZineContent();
   }
 
-  componentWillUnmount = () => {
-  }
-
-  onMouseMove(e) {
-  }
-
-  handleOver = (topic) => {
-  }
-
-  handleOut = () => {
+  componentWillUnmount() {
   }
 
   loadZineContent = () => {
-    const right = document.getElementById('image-right');
-    const left = document.getElementById('image-left');
+    // const right = document.getElementById('image-right');
+    // const left = document.getElementById('image-left');
     console.log(this.state);
-  }
+  };
 
   render() {
     return (
       <div id="random">
         <div id="random--container">
+          <div id="random--navigation">
+            <div id="navigtion--right">
+              <p>{'<'}</p>
+            </div>
+            <div id="navigtion--left">
+              <p>{'>'}</p>
+            </div>
+          </div>
           <div id="image--container">
-            <div id="image-right" />
-            <div id="image-left" />
-            {/* <div id="info--container">
-              <div id="info">
-                  <p>info</p>
+            <div id="image-right">
+              <div id="info--container">
+                <div id="info">
+                  <p>Add copy here...</p>
+                </div>
               </div>
-            </div> */}
-
+            </div>
+            <div id="image-left">
+              <div id="info--container">
+                <div id="info">
+                  <p>Add copy here...</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
